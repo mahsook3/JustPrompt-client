b7194525ebb254e06e1a39b3d3d1df46a2baec73
mahsook3 <mahsook.m.a@gmail.com>
2024-07-31 20:27:31 +0530

    chore: Update Achievement and Hero components

--- src/components/HomeNaveBar.js
+++ src/components/HomeNaveBar.js
@@ -59,7 +59,7 @@ export default function Navbar() {
         <div className="flex items-center lg:order-2">
           <a
             href="/login"
-            className="bg-green-400 hover:bg-green-600 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none text-gray-700"
+            className="bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none text-white"
           >
             Get Started
           </a>
